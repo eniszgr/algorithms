15e4bb68938081412194d603e1b7234a690856f2
eniszgr <117929152+eniszgr@users.noreply.github.com>
2022-11-18 01:26:35 +0300

    mission_2

---
+++ mission_2.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+main()
+{
+	int a=1,b=2,c=3,t=2;
+	printf("%d, %d, %d,",a,b,c);
+	while(c<4000000){
+		a=b;
+		b=c;
+		c=a+b;
+		if(c<4000000){
+			printf(",%d",c);
+			if(c%2==0){
+				t=t+c;
+			}
+		}
+	}
+	printf("\ntoplam %d",t);
+}
