c01d47b02ff91101e69b3a303578d2af5d7923b3
Enis ÖZGÜR <117929152+eniszgr@users.noreply.github.com>
2023-01-18 18:51:28 +0300

    Add files via upload

---
+++ mission_5.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+int divisible(unsigned number);
+main()
+{
+	unsigned number=20;
+	while (1)
+	{
+		if (divisible (number)==0)
+		{
+			number++;
+		}
+		else if(divisible (number)==1)
+		{
+			break;
+		}
+	}
+	printf("%u",number);
+	
+}
+int divisible (unsigned number)
+{
+	int i;
+	for (i=1;i<=20;i++)
+	{
+		if (number%i!=0)
+		{
+			return 0;
+		}
+	}
+	return 1;
+}
+
