1585cf9c94400a3b8a38e2d526015fb67b48d767
Enis ÖZGÜR <117929152+eniszgr@users.noreply.github.com>
2024-04-09 15:13:14 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace euler20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 100;
+            long sumofdigits = 0;
+            BigInteger sum = 1;
+            //find factorial
+            for (int i = num; i >= 1; i--)
+            {
+                sum = sum * i;
+            }
+            
+            //data type conversion
+            string number = sum.ToString();
+            
+            //access each number
+            foreach (char c in number)
+            {
+                sumofdigits += (int)char.GetNumericValue(c);
+            }
+            Console.WriteLine("equals = " + sumofdigits);
+            Console.ReadLine();
+        }
+       
+    }
+}
